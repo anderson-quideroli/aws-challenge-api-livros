2bab351cc1d45c823393ed5fa061a4a81e8bc46d
anderson-quideroli <anderson.quideroli@hotmail.com>
2026-01-08 12:36:26 -0300

    teste

--- TERRAFORM/s3.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_s3_bucket" "codedeploy_bucket" {
-  bucket = "codedeploy-api-livros-${local.account_id}"
-  force_destroy = true
-
-  tags = {
-    Name        = "codedeploy-api-livros"
-    Environment = "Production"
-  }
-}
